4f6d816a830b236a70ff2afe7d486728bb5af078
Daniel Furtado <73434420+danielfurtado11@users.noreply.github.com>
2025-03-03 13:15:59 +0000

    Add files via upload

---
+++ pages/web1 copy 3.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Relatório: Reunião NEXI (20-12-2024)")
+st.write("Aqui está o conteúdo detalhado do relatório...")
+
+if st.button("Voltar"):
+    st.switch_page("main.py")
